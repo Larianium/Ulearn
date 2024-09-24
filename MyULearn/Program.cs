3efb395b4bcc6edd5b4691fce59e04c9f4699f87
Lev Aleshkin <lev.aleshkin01@yandex.ru>
2024-09-24 14:52:47 +0300

    CommitUlearn_24092024_1452

--- MyULearn/Program.cs
+++ MyULearn/Program.cs
@@ -14,7 +14,25 @@ value_2 = value_1 - value_2;
 Console.WriteLine($"После: Значение 1 - {value_1}, Значение 2 - {value_2}");
 
 //Задача 2: Задается натуральное трехзначное число (гарантируется, что трехзначное). Развернуть его, т.е. получить трехзначное число, записанное теми же цифрами в обратном порядке.
-int threeDigitNaturNumb;
+int threeDigitNaturNumb = 719,
+    numb1,
+    numb2,
+    numb3,
+    dozen = 10, 
+    hundreds = 100,   
+    reverseDigitNaturNumb;
+
+
+numb1 = threeDigitNaturNumb / hundreds;
+numb2 = threeDigitNaturNumb % hundreds / dozen;
+numb3 = threeDigitNaturNumb % dozen;
+
+reverseDigitNaturNumb = numb3 * hundreds + numb2 * dozen + numb1;
+
+Console.WriteLine($"Первоначальное трёхзначное число: {threeDigitNaturNumb}, Число в обратном порядке - {reverseDigitNaturNumb}");
+
+
+
 
 
 
